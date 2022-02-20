c26505d1b1c76afe5eecb784cb45247985c4227e
RaidenAlfares13 <99805736+RaidenAlfares13@users.noreply.github.com>
2022-02-20 17:47:44 +0700

    Add files via upload

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -57,7 +57,7 @@ class ClientPrefs {
 
 	public static function saveSettings() {
 		FlxG.save.data.downScroll = downScroll;
-		FlxG.save.data.hitSound = hitsSound;
+		FlxG.save.data.hitSound = hitSound;
 		FlxG.save.data.middleScroll = middleScroll;
 		FlxG.save.data.showFPS = showFPS;
 		FlxG.save.data.flashing = flashing;
