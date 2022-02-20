0305ebf3056a29301b150cf8781817d9bce39e36
RaidenAlfares13 <99805736+RaidenAlfares13@users.noreply.github.com>
2022-02-20 11:25:04 +0700

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,4 +1,4 @@
-﻿package;
+package;
 
 #if desktop
 import Discord.DiscordClient;
@@ -69,16 +69,7 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['', 0.2], //From 0% to 19%
-		['', 0.4], //From 20% to 39%
-		['', 0.5], //From 40% to 49%
-		['', 0.6], //From 50% to 59%
-		['', 0.69], //From 60% to 68%
-		['', 0.7], //69%
-		['', 0.8], //From 70% to 79%
-		['', 0.9], //From 80% to 89%
-		['', 1], //From 90% to 99%
-		['', 1] //The value on this one isn't used actually, since Perfect is always "1"
+
 	];
 	
 	#if (haxe >= "4.0.0")
@@ -1980,7 +1971,7 @@ class PlayState extends MusicBeatState
 		if(ratingString == '?') {
 			scoreTxt.text = 'Score: ' + songScore + ' | Combo Breaks: ' + songMisses + ' | Accuracy: ' + ratingString;
 		} else {
-			scoreTxt.text = 'Score: ' + songScore + ' | Combo Breaks: ' + songMisses + ' | Accuracy: ' + ratingString + ' (' + Math.floor(ratingPercent * 250) + '%)';
+			scoreTxt.text = 'Score: ' + songScore + ' | Combo Breaks: ' + songMisses + ' | Accuracy: ' + ratingString + ' (' + Math.floor(ratingPercent * 250 + '%)';
 		}
 
 		if(cpuControlled) {
