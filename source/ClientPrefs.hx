4ac0e09760607376b486c72fca5fa0aeb372a73b
RaidenAlfares13 <99805736+RaidenAlfares13@users.noreply.github.com>
2022-02-21 17:47:29 +0700

    Add files via upload

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -8,10 +8,10 @@ import Controls;
 
 class ClientPrefs {
 	public static var downScroll:Bool = false;
-	public static var hitSound:Bool = false;
-	public static var middleScroll:Bool = false;
-	public static var showFPS:Bool = true;
-	public static var flashing:Bool = true;
+	public static var hitSound:Bool = true;
+	public static var middleScroll:Bool = true;
+	public static var showFPS:Bool = false;
+	public static var flashing:Bool = false;
 	public static var globalAntialiasing:Bool = true;
 	public static var noteSplashes:Bool = true;
 	public static var lowQuality:Bool = false;
